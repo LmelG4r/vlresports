f7c90e96a122c888859d6a99bbee3e6b18631400
Luis Melgar <melgar.vargas.luis.angel@gmail.com>
2025-01-18 18:35:46 -0600

    ddd

--- src/versions/v1/routes/resultados.js
+++ src/versions/v1/routes/resultados.js
@@ -1,8 +1,8 @@
 // src/versions/v1/routes/matchesr.js
 
 const express = require("express");
-const router = express.Router();
-const resultadosController = require("../../controllers/resultadosController");  // Verifica el nombre correcto
+const router = Router();
+const resultadosController = require("../../../controllers/resultadosController");  // Verifica el nombre correcto
 
 // Ruta para obtener los detalles de un partido
 router.get("/:id", resultadosController.getMatchDetails);
